5f5b81c3862ffa890153aa650f7aa381d7e16c33
nikkeykumar <nikkeykumar161@gmail.com>
2025-06-28 01:23:03 +0530

    url update

--- Backend/connection/connectionDB.js
+++ Backend/connection/connectionDB.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectionDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/job_portal");
+    await mongoose.connect("mongodb://localhost:27017/job_portal");
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("error to connecting mongodb ", error.message);
